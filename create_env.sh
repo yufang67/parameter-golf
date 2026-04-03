7fb860fc5f20beeaddb8c068336561236163f9f8
Yu Fang <fangyu67@gmail.com>
2026-04-03 22:08:46 +0000

    wip

--- create_env.sh
+++ create_env.sh
@@ -3,3 +3,5 @@ source .venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python3 data/cached_challenge_fineweb.py --variant sp1024
+git config --global user.email "fangyu67@gmail.com"
+git config --global user.name "Yu Fang"
